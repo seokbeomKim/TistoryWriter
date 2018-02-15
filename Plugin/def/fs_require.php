fa931b0af7ceec30ef39f6139682efa6a8b486e0
Sukbeom Kim <sukbeom.kim@gmail.com>
2018-02-16 02:13:12 +0900

    separator 수정

--- Plugin/def/fs_require.php
+++ Plugin/def/fs_require.php
@@ -8,13 +8,13 @@
 
 namespace tistory_writer;
 
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'option_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'page_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'class_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'script_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'api_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'auth_manager.php');
-require_once(PLUGIN_DIR . '\features' . DIRECTORY_SEPARATOR . 'handler_manager.php');
-require_once(PLUGIN_DIR . '\widgets' . DIRECTORY_SEPARATOR . 'TistoryMetabox.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'option_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'page_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'class_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'script_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'api_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'auth_manager.php');
+require_once(PLUGIN_DIR . 'features' . DIRECTORY_SEPARATOR . 'handler_manager.php');
+require_once(PLUGIN_DIR . 'widgets' . DIRECTORY_SEPARATOR . 'TistoryMetabox.php');
 
-require_once(PLUGIN_DIR . '\etc' . DIRECTORY_SEPARATOR . 'logger.php');
+require_once(PLUGIN_DIR . 'etc' . DIRECTORY_SEPARATOR . 'logger.php');
