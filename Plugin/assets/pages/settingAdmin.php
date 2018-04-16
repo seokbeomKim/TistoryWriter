b18d8a59d09d03331491cb54402182501b2d5040
Sukbeom Kim <sukbeom.kim@gmail.com>
2018-04-17 00:53:38 +0900

    Logger 호출 부분 정리(타 플러그인 영향 성 제거)

--- Plugin/assets/pages/settingAdmin.php
+++ Plugin/assets/pages/settingAdmin.php
@@ -145,7 +145,6 @@ namespace tistory_writer;
                         $optionMgr = TistoryWriter::getManager(FEATURE_KEY\OPTION);
                         $access_token = $optionMgr->getOption(OPTION_KEY\ACCESS_TOKEN);
                         $apiMgr = TistoryWriter::getManager(FEATURE_KEY\TISTORY_API);
-                        Logger::log("access token in main: " . $access_token);
 
                         if (isset($access_token) && $apiMgr->checkAccessToken()) {
                             echo $access_token;
